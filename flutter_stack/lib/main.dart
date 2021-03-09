11b46873e893645f69e027fbe02fa13ff6d6e5f4
DESKTOP-8SUA83E\Ali <rahemanali321@gmail.com>
2021-03-09 13:31:43 +0530

    flutter

---
+++ flutter_stack/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+       title: 'APP 10',
+       home: Scaffold(
+         appBar: AppBar(
+           title:Text("Day 10"),
+
+           ),
+           body: Center(
+             child: Stack(
+               fit: StackFit.passthrough,
+               overflow: Overflow.visible,
+               children: [
+                 Container(
+                   height: 300,
+                   width: 400,
+                   color: Colors.green,
+                   child: Center(
+                     child: Text(
+                          "Hello",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                     ),
+                   ),
+                 ),
+               ],
+             ),
+             ),
+       ),
+       
+    );
+  }
+}
