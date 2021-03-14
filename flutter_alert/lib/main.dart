4fdbd54b54726659cbd05df724f05c0719d3a66f
DESKTOP-0206L39\Ali <rahemanali321@gmail.com>
2021-03-14 15:25:30 -0700

    alert

--- flutter_alert/lib/main.dart
+++ flutter_alert/lib/main.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ALertWidget(),
+    );
+  }
+}
+
+class ALertWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
