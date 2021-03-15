06d0ca4f45595c1b1cd2e390b246fc56cbadb925
DESKTOP-0206L39\Ali <rahemanali321@gmail.com>
2021-03-15 14:00:42 -0700

    flutter is future

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -9,24 +9,68 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
-
 class _MyAppState extends State<MyApp> {
-  Widget button(String btntext) {
+  String output = "0";
+  String _output = "0";
+  double num1 = 0;
+  double num2 = 0;
+  String operand = "";
+
+  operation(String btnText) {
+    if (btnText == "C") {
+      _output = "0";
+      num1 = 0;
+      num2 = 0;
+      operand = "";
+    } else if (btnText == "+" ||
+        btnText == "-" ||
+        btnText == "*" ||
+        btnText == "/") {
+      num1 = double.parse(output);
+      operand = btnText;
+      _output = "0";
+    } else if (btnText == "=") {
+      num2 = double.parse(output);
+      if (operand == "+") {
+        _output = (num1 + num2).toString();
+      }
+      if (operand == "-") {
+        _output = (num1 - num2).toString();
+      }
+      if (operand == "*") {
+        _output = (num1 * num2).toString();
+      }
+      if (operand == "/") {
+        _output = (num1 / num2).toString();
+      }
+
+      num1 = 0;
+      num2 = 0;
+      operand = "";
+    } else {
+      _output = _output + btnText;
+    }
+
+    setState(() {
+      output = double.parse(_output).toStringAsFixed(2);
+    });
+  }
+
+  Widget button(String btnText) {
     return Expanded(
       child: RawMaterialButton(
-        shape: Border.all(color: Colors.black, width: 2),
-        fillColor: Colors.blueGrey,
-        splashColor: Colors.black,
-        padding: EdgeInsets.all(30.0),
         child: Text(
-          "$btntext",
+          "$btnText",
           style: TextStyle(
-            fontSize: 20,
-            color: Colors.white,
-            fontWeight: FontWeight.w600,
-          ),
+              fontSize: 30, color: Colors.white, fontWeight: FontWeight.w600),
         ),
-        onPressed: () {},
+        splashColor: Colors.black,
+        fillColor: Colors.blueGrey,
+        padding: EdgeInsets.all(20),
+        shape: Border.all(color: Colors.black, width: 2),
+        onPressed: () {
+          operation(btnText);
+        },
       ),
     );
   }
@@ -34,30 +78,33 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text("Calculator"),
           backgroundColor: Colors.blueGrey,
         ),
         body: Container(
+          margin: EdgeInsets.only(bottom: 20),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
               Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black, width: 2),
-                    color: Colors.black12,
-                  ),
-                  margin: EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    "OUTPUT",
-                    style: TextStyle(fontSize: 60, fontWeight: FontWeight.w600),
-                  ),
-                  alignment: Alignment.bottomRight,
-                  padding: EdgeInsets.all(20),
+                  child: Container(
+                child: Text(
+                  "$output",
+                  style: TextStyle(fontSize: 50, fontWeight: FontWeight.w500),
                 ),
-              ),
+                alignment: Alignment.bottomRight,
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 2,
+                    ),
+                    color: Colors.black12),
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.only(bottom: 8),
+              )),
               Row(
                 children: [
                   button("C"),
@@ -72,12 +119,10 @@ class _MyAppState extends State<MyApp> {
                   SizedBox(
                     width: 5,
                   ),
-                  button("/"),
+                  button("/")
                 ],
               ),
-              SizedBox(
-                height: 8,
-              ),
+              SizedBox(height: 8),
               Row(
                 children: [
                   button("9"),
@@ -92,12 +137,10 @@ class _MyAppState extends State<MyApp> {
                   SizedBox(
                     width: 5,
                   ),
-                  button("+"),
+                  button("+")
                 ],
               ),
-              SizedBox(
-                height: 8,
-              ),
+              SizedBox(height: 8),
               Row(
                 children: [
                   button("6"),
@@ -112,12 +155,10 @@ class _MyAppState extends State<MyApp> {
                   SizedBox(
                     width: 5,
                   ),
-                  button("-"),
+                  button("-")
                 ],
               ),
-              SizedBox(
-                height: 8,
-              ),
+              SizedBox(height: 8),
               Row(
                 children: [
                   button("3"),
@@ -132,7 +173,7 @@ class _MyAppState extends State<MyApp> {
                   SizedBox(
                     width: 5,
                   ),
-                  button("="),
+                  button("=")
                 ],
               ),
             ],
