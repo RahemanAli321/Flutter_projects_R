0ebc16c1991113ffd0aed43bc79ab58163c3af48
DESKTOP-0206L39\Ali <rahemanali321@gmail.com>
2021-03-16 04:54:35 -0700

    flutter

--- progressbar_snackbar/lib/main.dart
+++ progressbar_snackbar/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("progress bar"),
         ),
-        body: Linear(),
+        body: Snack_bar(),
       ),
     );
   }
@@ -31,17 +31,60 @@ class _LinearState extends State<Linear> {
     return Scaffold(
       body: Column(
         children: [
-          Container(
-            child: loading
-                ? LinearProgressIndicator()
-                : Text("please click to..."),
+          Center(
+            child: Container(
+              margin: EdgeInsets.all(150),
+              child: loading
+                  ? LinearProgressIndicator()
+                  : Text("please click to..."),
+            ),
           ),
           RaisedButton(
             child: Text("download"),
-            color: Colors.blue,
-            onPressed: () {},
+            color: Colors.red,
+            onPressed: () {
+              setState(() {
+                loading = !loading;
+              });
+            },
           ),
         ],
+      ),
+    );
+  }
+}
+
+class Circular extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: CircularProgressIndicator(
+          strokeWidth: 2,
+          backgroundColor: Colors.red,
+        ),
+      ),
+    );
+  }
+}
+
+class Snack_bar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: RaisedButton(
+        child: Text("Snack bar"),
+        color: Colors.red,
+        onPressed: () {
+          final msg = SnackBar(
+            content: Text("this"),
+            duration: Duration(
+              seconds: 1,
+            ),
+          );
+          Scaffold.of(context).showSnackBar(msg);
+        },
       ),
     );
   }
